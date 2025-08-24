5868b5f97ba04834315f21f6b8b53b1423369cef
Sistla Keerthana <keerthanasistla10@gmail.com>
2025-08-24 23:00:16 +0530

    SetMatrixZero

--- Day 1/SetMatrixZero.py
+++ Day 1/SetMatrixZero.py
@@ -2,37 +2,18 @@ from typing import List
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        first_row_zero = False
-        first_col_zero = False
-        
+        x = set()
+        y = set()
         for i in range(len(matrix)):
-            if matrix[i][0] == 0:
-                first_col_zero = True
-                break
-        
-        for j in range(len(matrix[0])):
-            if matrix[0][j] == 0:
-                first_row_zero = True
-                break
-        
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
+            for j in range(len(matrix[i])):
                 if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    matrix[0][j] = 0
-        
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    x.add(i)
+                    y.add(j)
+                    
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i in x or j in y:
                     matrix[i][j] = 0
-        
-        if first_col_zero:
-            for i in range(len(matrix)):
-                matrix[i][0] = 0
-        
-        if first_row_zero:
-            for j in range(len(matrix[0])):
-                matrix[0][j] = 0
 
 if __name__ == "__main__":
     matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
